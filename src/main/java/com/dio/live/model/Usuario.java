f38b1785af7457bffc826d29fbd27085d830d963
mardbrito <marcelo4web@gmail.com>
2021-08-17 19:56:37 -0400

    Primeiro commit do Projeto 1 da DIO

---
+++ src/main/java/com/dio/live/model/Usuario.java
@@ -0,0 +1,35 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne
+    private CategoriaUsuario categoriaUsuario;
+    private String nome;
+    @ManyToOne
+    private Empresa empresa;
+    @ManyToOne
+    private NivelAcesso nivelAcesso;
+    @ManyToOne
+    private JornadaTrabalho jornadaTrabalho;
+    private BigDecimal tolerancia;
+    private LocalDateTime inicioJoranada;
+    private LocalDateTime finalJoranada;
+}
